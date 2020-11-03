f0a31819b90e248945dafaa151c93dd208c15a42
VirtualFox <boris.fischer@student.vinci.be>
2020-11-03 14:23:36 +0100

    refactor - optimization countHours for 2 and 7

--- BerlinClock.php
+++ BerlinClock.php
@@ -40,7 +40,7 @@ class BerlinClock {
 
     public function countHours(int $int): string {
         if($int%5 === 1) return "RNNN";
-        if($int === 2 || $int === 7) return "RRNN";
+        if($int%5 === 2) return "RRNN";
         if($int === 3) return "RRRN";
         if($int === 4) return "RRRR";
         if($int%5 === 0) return "NNNN";
